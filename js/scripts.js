6444098a883a152d4751d361f8d5cfc4673057fb
IOINITID <50173108+IOINITID@users.noreply.github.com>
2019-08-28 14:28:43 +0300

    Added slides block

--- js/scripts.js
+++ js/scripts.js
@@ -20,3 +20,13 @@ $(".tab-three").click(() => {
   $(".tab-one").removeClass("active-tab");
   $(".tab-two").removeClass("active-tab");
 });
+
+$('.single-item').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 5000,
+  arrows: true,
+  dots: true
+});
+
