bf11de87cfd3ca37d9c7cee3c0c6890d2649980d
IOINITID <50173108+IOINITID@users.noreply.github.com>
2019-08-28 16:52:05 +0300

    Added footer block

--- js/scripts.js
+++ js/scripts.js
@@ -30,3 +30,11 @@ $('.single-item').slick({
   dots: true
 });
 
+$('.second-slider').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 5000,
+  arrows: true,
+  dots: false
+});
