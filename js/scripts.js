31550b99ecbad9a63cb86d39f04e28060eba71c8
IOINITID <50173108+IOINITID@users.noreply.github.com>
2019-08-29 12:00:52 +0300

    Added forms

--- js/scripts.js
+++ js/scripts.js
@@ -54,3 +54,17 @@ $('.second-slider').slick({
     }
   }]
 });
+// Mobile menu
+$('.btn_mobile').on('click', function () {
+  $('#menu').show('drop', 300, function () {
+    $('#panel').addClass('blur');
+  });
+  $('#panel').addClass('menuOpen');
+});
+// Mobile menu
+$('#mMenuClose').on('click', function () {
+  $('#menu').hide('drop', 300, function () {
+    $('#panel').removeClass('menuOpen')
+  });
+  $('#panel').removeClass('blur');
+});
