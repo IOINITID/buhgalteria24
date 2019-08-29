951a5bb0007dfcf85d54c66215c31f43bd72a586
IOINITID <50173108+IOINITID@users.noreply.github.com>
2019-08-29 10:45:46 +0300

    Added mobile adaptation

--- js/scripts.js
+++ js/scripts.js
@@ -1,40 +1,56 @@
-//Tabs init function
+// Tabs init function
 $(function () {
   $("#tabs").tabs();
 });
-//Tab-one actions
+// Tab-one actions
 $(".tab-one").click(() => {
   $(".tab-one").addClass("active-tab");
   $(".tab-two").removeClass("active-tab");
   $(".tab-three").removeClass("active-tab");
 });
-//Tab-two actions
+// Tab-two actions
 $(".tab-two").click(() => {
   $(".tab-two").addClass("active-tab");
   $(".tab-one").removeClass("active-tab");
   $(".tab-three").removeClass("active-tab");
 });
-//Tab-three actions
+// Tab-three actions
 $(".tab-three").click(() => {
   $(".tab-three").addClass("active-tab");
   $(".tab-one").removeClass("active-tab");
   $(".tab-two").removeClass("active-tab");
 });
-
-$('.single-item').slick({
+// Slider one
+$('.first-slider').slick({
   slidesToShow: 1,
   slidesToScroll: 1,
   autoplay: true,
   autoplaySpeed: 5000,
   arrows: true,
-  dots: true
+  dots: true,
+  responsive: [{
+    breakpoint: 576,
+    settings: {
+      arrows: false,
+      slidesToShow: 1,
+      dots: false
+    }
+  }]
 });
-
+// Slider two
 $('.second-slider').slick({
   slidesToShow: 1,
   slidesToScroll: 1,
   autoplay: true,
   autoplaySpeed: 5000,
   arrows: true,
-  dots: false
+  dots: false,
+  responsive: [{
+    breakpoint: 576,
+    settings: {
+      arrows: false,
+      slidesToShow: 1,
+      dots: false
+    }
+  }]
 });
